72e3244d78c58584c1373cabd635728d702cdcc0
xinqiang-hu <danxqhu@gmail.com>
2023-01-13 17:56:58 +0800

    add postcss

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,4 +3,13 @@ module.exports = {
     host: '127.0.0.1',
     port: 8800,
   },
+  // https://www.youtube.com/watch?v=SH6Y_MQzFVw
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['css-loader'],
+      },
+    ],
+  },
 };
